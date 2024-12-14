db561c33fab2ed256555723e9a2fac7bac9c0c2e
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2024-12-14 23:24:58 +0100

    Working with smtplib

--- 32/Birthday Wisher (Day 32) start/main.py
+++ 32/Birthday Wisher (Day 32) start/main.py
@@ -1,1 +1,12 @@
+import smtplib
 
+sender_email = "s.jalalabbasi37@gmail.com"
+password = "ckqwsmwawcqtafcr"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=sender_email, password=password)
+    connection.sendmail(from_addr=sender_email,
+                        to_addrs="s.jalal9s@yahoo.com",
+                        msg="Subject:Hi \n\n This is my message for you bro."
+                        )
