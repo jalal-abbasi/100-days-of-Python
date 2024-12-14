94a99a33849b551915f3b394351e450c3d75ede4
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2024-12-14 23:43:23 +0100

    Working with datetime module

--- 32/Birthday Wisher (Day 32) start/main.py
+++ 32/Birthday Wisher (Day 32) start/main.py
@@ -1,12 +1,23 @@
-import smtplib
+# import smtplib
+#
+# sender_email = "s.jalalabbasi37@gmail.com"
+# password = "ckqwsmwawcqtafcr"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=sender_email, password=password)
+#     connection.sendmail(from_addr=sender_email,
+#                         to_addrs="s.jalal9s@yahoo.com",
+#                         msg="Subject:Hi \n\n This is my message for you bro."
+#                         )
 
-sender_email = "s.jalalabbasi37@gmail.com"
-password = "ckqwsmwawcqtafcr"
+import datetime as dt
+current_time = dt.datetime.now()
+print(current_time)
+current_year = current_time.year
+current_day = current_time.day
+current_weekday = current_time.weekday()
+print(current_weekday)
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=sender_email, password=password)
-    connection.sendmail(from_addr=sender_email,
-                        to_addrs="s.jalal9s@yahoo.com",
-                        msg="Subject:Hi \n\n This is my message for you bro."
-                        )
+my_datetime = dt.datetime(2024, 12, 1)
+print(my_datetime)
