ec30eb19edd1c06f362400788e1cf00fc5c89e31
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2024-12-15 00:21:22 +0100

    Challenge 1: Sending motivational quotes to my email every sundays

--- 32/Birthday Wisher (Day 32) start/main.py
+++ 32/Birthday Wisher (Day 32) start/main.py
@@ -1,23 +1,34 @@
-# import smtplib
-#
-# sender_email = "s.jalalabbasi37@gmail.com"
-# password = "ckqwsmwawcqtafcr"
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=sender_email, password=password)
-#     connection.sendmail(from_addr=sender_email,
-#                         to_addrs="s.jalal9s@yahoo.com",
-#                         msg="Subject:Hi \n\n This is my message for you bro."
-#                         )
-
+import smtplib
 import datetime as dt
-current_time = dt.datetime.now()
-print(current_time)
-current_year = current_time.year
-current_day = current_time.day
-current_weekday = current_time.weekday()
-print(current_weekday)
+import random
 
-my_datetime = dt.datetime(2024, 12, 1)
-print(my_datetime)
+
+with open("quotes.txt", mode="r") as file:
+    quotes = file.readlines()
+
+motivational_sentence = random.choice(quotes)
+
+sundays_index = 6
+current_time = dt.datetime.now()
+current_weekday = current_time.weekday()
+
+sender_email = "s.jalalabbasi37@gmail.com"
+password = "ckqwsmwawcqtafcr"
+
+if current_weekday == sundays_index:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=sender_email, password=password)
+        connection.sendmail(from_addr=sender_email,
+                            to_addrs="s.jalal9s@yahoo.com",
+                            msg=f"Subject:Sundays Motivational Quote \n\n {motivational_sentence}"
+                            )
+
+# print(current_time)
+# current_year = current_time.year
+# current_day = current_time.day
+
+
+#
+# my_datetime = dt.datetime(2024, 12, 1)
+# print(my_datetime)
