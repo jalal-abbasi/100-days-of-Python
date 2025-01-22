3acd406c69f211cfe0bf782c42024890546e6df3
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-01-22 20:43:16 +0100

    bug fixing: update the ISS location every 60 seconds.

--- 33/Final project/main.py
+++ 33/Final project/main.py
@@ -8,17 +8,6 @@ MY_LONG = 9.189982 # Your longitude
 
 MY_POS = [MY_LAT, MY_LONG]
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-iss_position = (iss_latitude, iss_longitude)
-#Your position is within +5 or -5 degrees of the ISS position.
-
-
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
@@ -32,8 +21,10 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 sun_hours = (sunrise, sunset)
+
 time_now = datetime.now()
 current_hour = time_now.hour
+
 
 
 #If the ISS is close to my current position
@@ -49,8 +40,18 @@ def check_iss_is_visible(iss_position, my_position, sun_hours, current_hour):
             is_visible = 1
     return is_visible
 
+
 while True:
-    if check_iss_is_visible(iss_position, MY_POS, sun_hours, current_hour):
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    iss_position = (iss_latitude, iss_longitude)
+
+    if check_iss_is_visible(iss_position, MY_POS, sun_hours, current_hour=12):
         with smtplib.SMTP("smtp.gmail.com") as connection:
             sender_email = "s.jalalabbasi37@gmail.com"
             password = "ckqwsmwawcqtafcr"
@@ -59,7 +60,7 @@ while True:
             connection.login(user=sender_email, password=password)
             connection.sendmail(from_addr=sender_email,
                                 to_addrs="s.jalalabbasi37@gmail.com",
-                                msg=f"ISS is visible!! \n\n {message}"
+                                msg=f"Subject:ISS is visible!! \n\n {message}"
                                 )
     time.sleep(60)
 
