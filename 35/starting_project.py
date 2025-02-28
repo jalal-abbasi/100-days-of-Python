8485ec906f97eeff91cc186762058a91143a4845
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-03-01 00:47:09 +0100

    adding a user environment variable to secure keys from being stollen

--- 35/starting_project.py
+++ 35/starting_project.py
@@ -1,15 +1,17 @@
 import requests
 from twilio.rest import Client
+import os
 
 
 twilio_phone_number = "+18172413781"
-account_sid = "Arr4512e"
-auth_token = "1213"
+account_sid = "ACe34c895e1146f0a1b54f5e16d8c4512e"
+auth_token = os.environ.get("AUTH_TOKEN")
 
-api_key = "30b8331fb24765f13510612b4d8ae50e"
+api_key = os.environ.get("OWM_API_KEY")
+
 parameters = {
-    "lat": 37.177338,
-    "lon": -3.598557,
+    "lat": 45.463619,
+    "lon": 9.188120,
     "appid": api_key,
     "cnt": 4,
 }
@@ -26,7 +28,7 @@ for timestep in data["list"]:
 if will_rain:
     client = Client(account_sid, auth_token)
     message = client.messages.create(
-        body = "The weather will be shitty in the next 12 hours, bring your fucking umbrella!",
+        body = "The weather will be rainy in the next 12 hours, bring your umbrella!",
         from_ = twilio_phone_number,
         to = "+393428399994",
     )
