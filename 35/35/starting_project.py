eec7200536343da840a877365a6afb729769b884
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-02-28 20:05:21 +0100

    writting a program to notify us if there will be rain in the next 12 hours.

--- 35/35/starting_project.py
+++ 35/35/starting_project.py
@@ -2,14 +2,20 @@ import requests
 
 api_key = "30b8331fb24765f13510612b4d8ae50e"
 parameters = {
-    "lat": 45.464203,
-    "lon": 9.189982,
+    "lat": 45.463619,
+    "lon": 9.188120,
     "appid": api_key,
+    "cnt": 4,
 }
-response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters)
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
 
 data = response.json()
-print(data)
 
+will_rain = False
+for timestep in data["list"]:
+    if int(timestep["weather"][0]["id"]) < 700:
+        will_rain = True
 
+if will_rain:
+    print("the weather will be shitty in the next 12 hours, bring your fucking umbrella!")
