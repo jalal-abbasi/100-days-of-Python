0d770b80cc4277813c689329008b3927cab8b7e8
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-03-01 22:42:53 +0100

    Finishing the code

--- 36/stock-news-hard-start/main.py
+++ 36/stock-news-hard-start/main.py
@@ -3,6 +3,7 @@ from urllib.parse import urlencode
 import requests
 import os
 import datetime
+from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -12,6 +13,10 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 NEWS_API = os.environ.get("NEWS_API_KEY")
 STOCK_API = os.environ.get("ALFA_VANTAGE_API")
+
+twilio_phone_number = "+14155238886"
+account_sid = "ACe34c895e1146f0a1b54f5e16d8c4512e"
+auth_token = os.environ.get("AUTH_TOKEN")
 
 def calculate_change_percentage(yesterday_value, day_before_yesterday_value):
     difference = yesterday_value - day_before_yesterday_value
@@ -29,22 +34,23 @@ stock_parameters = {
     "symbol" : "TSLA",
     "apikey" : STOCK_API,
 }
-# response = requests.get(STOCK_ENDPOINT, params=stock_parameters)
-# response.raise_for_status()
-# data = response.json()
-#
-# today = datetime.date.today()
-# delta = datetime.timedelta(days=1)
-# yesterday = today - delta
-# day_before_yesterday = yesterday - delta
-#
-# yesterday = str(yesterday)
-# day_before_yesterday = str(day_before_yesterday)
-#
-# yesterday_cv = float(data["Time Series (Daily)"][yesterday]['4. close'])
-# day_before_yesterday_cv = float(data["Time Series (Daily)"][day_before_yesterday]['4. close'])
-#
-# is_high_change, change_percentile = calculate_change_percentage(yesterday_cv, day_before_yesterday_cv)
+
+response = requests.get(STOCK_ENDPOINT, params=stock_parameters)
+response.raise_for_status()
+data = response.json()
+
+today = datetime.date.today()
+delta = datetime.timedelta(days=1)
+yesterday = today - delta
+day_before_yesterday = yesterday - delta
+
+yesterday = str(yesterday)
+day_before_yesterday = str(day_before_yesterday)
+
+yesterday_cv = float(data["Time Series (Daily)"][yesterday]['4. close'])
+day_before_yesterday_cv = float(data["Time Series (Daily)"][day_before_yesterday]['4. close'])
+
+is_high_change, change_percentile = calculate_change_percentage(yesterday_cv, day_before_yesterday_cv)
 
 
 
@@ -52,7 +58,7 @@ stock_parameters = {
 ## STEP 2: Use https://newsapi.org/docs/endpoints/everything
 # Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
 #HINT 1: Think about using the Python Slice Operator
-if True:
+if is_high_change:
     news_parameters = {
         "q" : "Tesla",
         "pageSize" : 3,
@@ -60,31 +66,39 @@ if True:
     }
     response = requests.get(NEWS_ENDPOINT, params=news_parameters)
     response.raise_for_status()
+    data = response.json()
 
-    news_list = []
-    for index in range(0, 2):
-        data = response.json()
-        title = data["articles"][index]["title"]
-        url = data["articles"][index]["url"]
+    for i in range(0,2):
+        title = data["articles"][i]["title"]
+        url = data["articles"][i]["url"]
 
-        description = (data["articles"][index]["description"])
+        description = (data["articles"][i]["description"])
+        index = description.index(".")
+        description = description[0:index +1]
 
         news = {
-            "Title" : title,
+            "Headline" : title,
             "url" : url,
-            "Description" : description,
+            "Brief" : description,
         }
-        news_list.append(news)
 
-print(news_list)
-
-
+        client = Client(account_sid, auth_token)
+        if change_percentile > 0:
+            arrow = "🔺"
+        else:
+            arrow = "🔻"
+        message = client.messages.create(
+            body=f"TSLA: {arrow}{abs(change_percentile)}%\n"
+                 f"Headline: {news["Headline"]}\nBrief: {news["Brief"]}\nURL: {news["url"]}",
+            from_=twilio_phone_number,
+            to="+393515024648",
+        )
+        print(message.status)
 
 
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 # Send a separate message with each article's title and description to your phone number. 
 #HINT 1: Consider using a List Comprehension.
-
 
 
 #Optional: Format the SMS message like this: 
