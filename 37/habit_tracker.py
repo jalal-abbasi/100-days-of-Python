ae34b20afe2033b30a65401b80f85ae8c8d439af
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-03-02 22:21:44 +0100

    updating a pixel on the graph made

--- 37/habit_tracker.py
+++ 37/habit_tracker.py
@@ -7,38 +7,46 @@ TOKEN = 'J10648899a!'
 HEADERS = {
     "X-USER-TOKEN" : TOKEN,
 }
-
-
-create_user_data = {
-    'token' : TOKEN ,
-    'username' : USERNAME,
-    'agreeTermsOfService' : 'yes',
-    'notMinor' : 'yes'
-}
-requests.post(url=create_user_url, json=create_user_data)
-
-
+#
+#
+# create_user_data = {
+#     'token' : TOKEN ,
+#     'username' : USERNAME,
+#     'agreeTermsOfService' : 'yes',
+#     'notMinor' : 'yes'
+# }
+# requests.post(url=create_user_url, json=create_user_data)
+#
+#
 create_graph_url = f"{create_user_url}/{USERNAME}/graphs"
+#
+# create_graph_data = {
+#     'id' : 'codingtracker',
+#     'name' : 'Coding Tracker',
+#     'unit' : 'minute',
+#     'type' : 'int',
+#     'color' : 'sora',
+# }
+#
+#
+# requests.post(url=create_graph_url, json=create_graph_data, headers=HEADERS)
 
-create_graph_data = {
-    'id' : 'codingtracker',
-    'name' : 'Coding Tracker',
-    'unit' : 'minute',
-    'type' : 'int',
-    'color' : 'sora',
-}
-
-
-response = requests.post(url=create_graph_url, json=create_graph_data, headers=HEADERS)
-print(response.text)
 
 today = datetime.now()
 yesterday = datetime(year=2025, month=3, day=1)
 create_pixel_url = f"{create_graph_url}/codingtracker"
-create_pixel_data = {
-    'date' : yesterday.strftime("%Y%m%d"),
-    'quantity' : '200',
+# create_pixel_data = {
+#     'date' : yesterday.strftime("%Y%m%d"),
+#     'quantity' : '200',
+# }
+#
+# requests.post(url=create_pixel_url, json=create_pixel_data, headers=HEADERS)
+
+update_pixel_url = f"{create_pixel_url}/{yesterday.strftime("%Y%m%d")}"
+
+update_pixel_data = {
+    'quantity' : '220'
 }
 
-response = requests.post(url=create_pixel_url, json=create_pixel_data, headers=HEADERS)
+response = requests.put(url=update_pixel_url, json=update_pixel_data, headers=HEADERS)
 print(response.text)
