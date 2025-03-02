ce108b3e9222cf01a5d86be8c5a97e93a859771c
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-03-02 22:14:27 +0100

    Creating a graph on pixela

--- 37/habit_tracker.py
+++ 37/habit_tracker.py
@@ -19,3 +19,16 @@ requests.post(url=create_user_url, json=create_user_data)
 
 
 create_graph_url = f"{create_user_url}/{USERNAME}/graphs"
+
+create_graph_data = {
+    'id' : 'codingtracker',
+    'name' : 'Coding Tracker',
+    'unit' : 'minute',
+    'type' : 'int',
+    'color' : 'sora',
+}
+
+
+response = requests.post(url=create_graph_url, json=create_graph_data, headers=HEADERS)
+print(response.text)
+
