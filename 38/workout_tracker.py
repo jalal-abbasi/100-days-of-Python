acb37627047d21c2e65461a83e5c99ea9b265f69
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-03-04 00:44:11 +0100

    Adding the sheety endpoint, header and parameters.

--- 38/workout_tracker.py
+++ 38/workout_tracker.py
@@ -1,15 +1,16 @@
 import requests
 import os
+from datetime import datetime
 
 
-ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+NUTRITION_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-app_id = os.environ.get("NUTRITION_APP_ID")
-app_key = os.environ.get("NUTRITION_APP_KEY")
+nutrition_app_id = os.environ.get("NUTRITION_APP_ID")
+nutrition_app_key = os.environ.get("NUTRITION_APP_KEY")
 
 headers = {
-    "x-app-id" : app_id,
-    "x-app-key" : app_key,
+    "x-app-id" : nutrition_app_id,
+    "x-app-key" : nutrition_app_key,
 }
 
 user_query = input("what exercise did you do today? ")
@@ -17,6 +18,23 @@ app_parameters = {
     "query" : user_query,
 }
 
-response = requests.post(url=ENDPOINT, json=app_parameters, headers=headers)
+response = requests.post(url=NUTRITION_ENDPOINT, json=app_parameters, headers=headers)
 
 print(response.text)
+
+SHEETY_ENDPOINT = "https://api.sheety.co/14a00bdc0fae03aacc590eb008c2c254/workoutTracking/workouts"
+sheety_header = {
+    "Authorization" : "Basic SmFsYWwgQWJiYXNpOkoxMDY0ODg5OWEh"
+}
+
+today = datetime.now()
+date = today.date()
+time = today.time()
+print(f"{date} \n {time}")
+sheety_parameters = {
+    "Date" : date.strftime("%d/%m/%Y"),
+    "Time" : time.strftime("%-I:%M:%S %p"),
+    "Exercise" : "",
+    "Duration" : "",
+    "Calories" : "",
+}
