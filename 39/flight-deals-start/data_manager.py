cb3b37ca624a7354a2e6360955a4e730797a4e08
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-03-05 00:29:37 +0100

    Writting DataManeger class

--- 39/flight-deals-start/data_manager.py
+++ 39/flight-deals-start/data_manager.py
@@ -1,4 +1,12 @@
+import requests
+import os
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    def __init__(self, api):
-        self.api_key = api
+    def __init__(self, body):
+        self.api_key = "https://api.sheety.co/14a00bdc0fae03aacc590eb008c2c254/flightDeals/prices"
+        self.body = body
+
+    def edit_row(self, data):
+        response = requests.put(url=self.api_key, json=data)
+        response.raise_for_status()
+        print(response.text)
