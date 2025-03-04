6eb547b823ac3dad7688c4eea259904cc88f4370
jalal-abbasi <72163298+jalal-abbasi@users.noreply.github.com>
2025-03-04 01:53:58 +0100

    Adding env variables

--- 38/workout_tracker.py
+++ 38/workout_tracker.py
@@ -3,28 +3,34 @@ import os
 from datetime import datetime
 
 
-# NUTRITION_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
-#
-# nutrition_app_id = os.environ.get("NUTRITION_APP_ID")
-# nutrition_app_key = os.environ.get("NUTRITION_APP_KEY")
-#
-# headers = {
-#     "x-app-id" : nutrition_app_id,
-#     "x-app-key" : nutrition_app_key,
-# }
-#
-# user_query = input("what exercise did you do today? ")
-# app_parameters = {
-#     "query" : user_query,
-# }
-#
-# response = requests.post(url=NUTRITION_ENDPOINT, json=app_parameters, headers=headers)
-#
-# print(response.text)
+NUTRITION_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-SHEETY_ENDPOINT = "https://api.sheety.co/14a00bdc0fae03aacc590eb008c2c254/workoutTracking/workouts"
+nutrition_app_id = os.environ.get("NUTRITION_APP_ID")
+nutrition_app_key = os.environ.get("NUTRITION_APP_KEY")
+
+headers = {
+    "x-app-id" : nutrition_app_id,
+    "x-app-key" : nutrition_app_key,
+}
+
+user_query = input("what exercise did you do today? ")
+app_parameters = {
+    "query" : user_query,
+}
+
+response = requests.post(url=NUTRITION_ENDPOINT, json=app_parameters, headers=headers)
+data = response.json()
+activity = data["exercises"][0]["name"]
+duration = data["exercises"][0]["duration_min"]
+nf_calories = data["exercises"][0]["nf_calories"]
+
+
+
+os.environ["SHEETY_ENDPOINT"] = "https://api.sheety.co/14a00bdc0fae03aacc590eb008c2c254/workoutTracking/workouts"
+sheety_endpoint = os.environ.get("SHEETY_ENDPOINT")
+
 SHEETY_HEADER = {
-    "Authorization" : "Basic SmFsYWwgQWJiYXNpOkoxMDY0ODg5OWEh"
+    "Authorization" : os.environ.get("SHEETY_HEADER")
 }
 
 today = datetime.now()
@@ -34,13 +40,13 @@ time = today.time()
 sheety_parameters = {
     "workout" : {
             "date" : date.strftime("%d/%m/%Y"),
-            "time" : time.strftime("%H:%M:%S"),
-            "exercise" : "running",
-            "duration" : "22",
-            "calories" : "130",
+            "time" : time.strftime("%I:%M:%S %p").lstrip("0"),
+            "exercise" : activity.title(),
+            "duration" : duration,
+            "calories" : nf_calories,
     }
 
 }
 
-sheety_response = requests.post(url=SHEETY_ENDPOINT, json=sheety_parameters, headers=SHEETY_HEADER)
+sheety_response = requests.post(url=sheety_endpoint, json=sheety_parameters, headers=SHEETY_HEADER)
 print(sheety_response.status_code)
